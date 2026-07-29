f2e4bb002f8b8d17fb9c07cf69e8cdd5e326311b
Maayan Matsliah <maayan.matsliah@gmail.com>
2026-07-29 14:42:52 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,31 @@
+'''
+understand:
+- input: numbers (array of ints, sorted in ascending order)
+         target: int
+- output: lst of 2 1- indexed indices where the vals add up to target
+
+- limitations: index1 != index2, o(1) memory, o(n) time
+
+plan:
+- slow pointer: starts at position 0
+- fast pointer: starts at position 1
+- length = len(numbers) - 1
+- if numbers[slow] + numbers[fast] = target return [slow + 1, fast + 1]
+- elif fast < length fast +=1
+- else slow +=1, fast = slow + 1
+'''
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        slow = 0
+        fast = 1
+        length = len(numbers) - 1
+
+        while True:
+            if numbers[slow] + numbers[fast] == target:
+                return [slow + 1, fast + 1]
+            elif fast < length:
+                fast +=1
+            else:
+                slow +=1
+                fast = slow + 1
+        
